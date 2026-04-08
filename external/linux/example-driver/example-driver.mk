6fa8f4c4a0ee1bf49c574e923afdff2f0e8e1f62
Kevin Aimaier <kflyn825@outlook.com>
2026-04-08 22:59:02 +0800

    minor fix on external dir

--- external/linux/example-driver/example-driver.mk
+++
@@ -1,7 +0,0 @@
-EXAMPLE_DRIVER_VERSION = local
-EXAMPLE_DRIVER_SITE = $(BR2_EXTERNAL_QEMUEMBEDDEDLINUX_PATH)/linux/example-driver/src
-EXAMPLE_DRIVER_SITE_METHOD = local
-
-# Assumes kernel-module infrastructure. Requires Kbuild/Makefile in src/
-$(eval $(kernel-module))
-$(eval $(generic-package))
--- external/linux/scelton/scelton.mk
+++
@@ -1,7 +0,0 @@
-SCELTON_VERSION = local
-SCELTON_SITE = $(BR2_EXTERNAL_QEMUEMBEDDEDLINUX_PATH)/linux/scelton/src
-SCELTON_SITE_METHOD = local
-
-# Assumes kernel-module infrastructure. Requires Kbuild/Makefile in src/
-$(eval $(kernel-module))
-$(eval $(generic-package))

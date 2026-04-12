aa809c3e4edb0219eac55668eb4ed9a784d98f0a
Kevin Aimaier <kflyn825@outlook.com>
2026-04-12 09:57:48 +0800

    convenianet aliases setup

---
+++ setup_alias.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+build_minimal_rv_virt() {
+    #export KBUILD_OUT_DIR="/mnt/wsl/ramdisk5/kbuild_out/riscv_virt_minimal_DEBUG_1"
+    export PROJECT_ROOT="/mnt/wsl/ramdisk5/QemuEmbeddedLinux"
+    export KERNEL_SRC_PATH="/mnt/wsl/ramdisk5/linux"
+    mkdir -p "/mnt/wsl/ramdisk5/kbuild_out/riscv"
+    cp "$PROJECT_ROOT/external/board/qemu_riscv64_virt_board/kernel_configs/riscv_virt_minimal_DEBUG_defconfig" "/mnt/wsl/ramdisk5/kbuild_out/riscv/.config"
+    ./kmake ARCH=riscv CROSS_COMPILE=riscv64-unknown-linux-gnu- olddefconfig
+    ./kmake ARCH=riscv CROSS_COMPILE=riscv64-unknown-linux-gnu- -j$(nproc) all
+
+    #return 0
+
+    qemu-system-riscv64 \
+    -M virt \
+    -cpu rv64 \
+    -m 1G \
+    -smp 4 \
+    -nographic \
+    -kernel /mnt/wsl/ramdisk5/kbuild_out/riscv/arch/riscv/boot/Image \
+    -append "console=ttyS0 root=/dev/vda ro earlycon" \
+    -drive file=/mnt/wsl/ramdisk5/out/qemu_riscv64_virt_board/customBuild/images/rootfs.ext4,format=raw,id=hd0,if=none \
+    -device virtio-blk-device,drive=hd0
+
+}
+
+
+build_minimal_rv_virt_0() {
+
+    export PROJECT_ROOT="/mnt/wsl/ramdisk5/QemuEmbeddedLinux"
+    #export KERNEL_SRC_PATH="/mnt/wsl/ramdisk5/linux"
+    mkdir -p "/mnt/wsl/ramdisk5/kbuild_out/riscv"
+    cp "$PROJECT_ROOT/external/board/qemu_riscv64_virt_board/kernel_configs/riscv_virt_minimal_DEBUG_defconfig" "/mnt/wsl/ramdisk5/kbuild_out/riscv/.config"
+    ./kmake ARCH=riscv CROSS_COMPILE=riscv64-unknown-linux-gnu- olddefconfig
+    ./kmake ARCH=riscv CROSS_COMPILE=riscv64-unknown-linux-gnu- -j$(nproc) all
+
+}
+
+
+#alias run_in_docker='docker run -it -e TERM=${TERM} --rm ${dockerVolumes} -w $(pwd -P) kflyn825/rvdev:latest bash -c'
+
+run_rv_virt() {
+
+
+    qemu-system-riscv64 \
+    -M virt \
+    -cpu rv64 \
+    -m 1G \
+    -smp 4 \
+    -display none \
+    -serial stdio \
+    -kernel /mnt/wsl/ramdisk5/kbuild_out_t0/riscv/arch/riscv/boot/Image \
+    -append "console=ttyS0 root=/dev/vda ro earlycon" \
+    -drive file=/mnt/wsl/ramdisk5/out/qemu_riscv64_virt_board/customBuild/images/rootfs.ext4,format=raw,id=hd0,if=none \
+    -device virtio-blk-device,drive=hd0
+}
